517f5f01fbddb6951113a66e2ae726d34cd69828
Darya Tishchenko QA-41 <tdaniel@bk.ru>
2023-02-06 16:05:31 +0400

    Add Test

--- src/test/java/ru/netology/cardDeliveryOrderTest.java
+++ src/test/java/ru/netology/cardDeliveryOrderTest.java
@@ -1,13 +1,23 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
 
 public class cardDeliveryOrderTest {
     private WebDriver driver;
@@ -30,8 +40,27 @@ public class cardDeliveryOrderTest {
         driver = null;
     }
 
+    String generationData(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
     @Test
     public void shouldSendSuccessfully() {
+        open("http://localhost:9999/");
+
+        $("[data-test-id=city] input").setValue("Москва");
+
+        String addDate = generationData(4);
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").setValue(addDate);
+
+        $("[data-test-id=name] input").setValue("Фомина Анна-Мария");
+
+        $("[data-test-id=phone] input").setValue("+79581234567");
+
+        $("[data-test-id=agreement]").click();
+        $$("button").find(text("Забронировать")).click();
+        $("[data-test-id=notification]").shouldHave(Condition.text("Успешно! " + "Встреча успешно забронирована на " + addDate), Duration.ofMillis(15000)).shouldBe(visible);
+
 
     }
 }
